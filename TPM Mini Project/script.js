ab390f6e8e514bd3db676d8e31b9e2f467b082c3
Alfian <alfian101@gmail.com>
2020-12-17 19:07:56 +0700

    added validation for form

--- TPM Mini Project/script.js
+++ TPM Mini Project/script.js
@@ -1,1 +1,39 @@
+$(document).ready(function () {
+  $("#input-form").validate({
+    // initialize the plugin
+    rules: {
+      name: {
+        required: true,
+        minlength: 4,
+      },
+      email: {
+        required: true,
+        email: true,
+        minlength: 7,
+      },
+      phonenumber: {
+        required: true,
+        minlength: 9,
+      },
+      message: {
+        required: true,
+        minlength: 15,
+      },
+    },
+  });
+});
 
+function alertFunction() {
+  alert("Form Submitted");
+}
+
+function validasi() {
+  var nama = document.getElementById("form-name").value;
+  var email = document.getElementById("form-email").value;
+  var alamat = document.getElementById("form-phone-number").value;
+  if (form - name != "" && form - email != "" && form - phone - number != "") {
+    alert("Form Submitted");
+  } else {
+    alert("Please fill in the required forms");
+  }
+}
